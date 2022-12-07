3cefec6776c4b4e4fede062de89fe7e621118677
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-07 12:10:43 +0000

    Finished CommentCard

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,14 +1,71 @@
+import styled from 'styled-components';
+
 const dayjs = require('dayjs');
 const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
+const CardLayout = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: #f0eded;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    gap: 0.5rem;
+`;
+
+const Information = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const BottomList = styled.ul`
+    display: flex;
+    /* list-style: none; */
+    padding: 0;
+    /* gap: 0.25rem; */
+`;
+
+const BottomSpan = styled.span`
+    ;
+`;
+
+const BottomItem = styled.li`
+    list-style-type: disc;
+    list-style-position: inside;
+    
+    &:first-child {
+        list-style: none;
+        > ${BottomSpan} {
+            margin-left: 0;
+        }
+    }
+
+    > ${BottomSpan} {
+        margin-left: -0.25rem;
+        margin-right: 0.75rem;
+    }
+
+    &::marker {
+        margin: 20rem;
+        /* color: red; */
+        text-indent: -20rem;
+    }
+`;
+
+
 const CommentCard = ({imageUrl, userName, datePosted, children}) => {
-    return <>
+    return <CardLayout>
         <img src={imageUrl} alt="comment"/>
-        <p>{userName}</p>
-        <p>{dayjs().to(dayjs(datePosted))}</p>
-        <p>{children}</p>
-    </>
+        <Information>
+            <a href="example.com">{userName}</a>
+            <p>{children}</p>
+            <BottomList>
+                <BottomItem><BottomSpan><a href="like">Like</a></BottomSpan></BottomItem>
+                <BottomItem><BottomSpan><a href="Reply">Reply</a></BottomSpan></BottomItem>
+                <BottomItem><BottomSpan>{dayjs().to(dayjs(datePosted))}</BottomSpan></BottomItem>
+            </BottomList>
+        </Information>
+    </CardLayout>
 }
 
 export default CommentCard;
